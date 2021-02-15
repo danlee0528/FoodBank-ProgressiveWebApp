38a3fde6f54e4c0d9497be5d1193ddd84275da38
Daniel Lee <dlee0528@hotmail.com>
2021-02-14 18:35:36 -0800

    pre cache assets

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/pages/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
 // install service worker
 self.addEventListener('install', evt =>{
-    console.log('service worker has been installed');
+    // console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            return cache.addAll(assets);
+        })
+    );
 });
 
 // activate service worker
