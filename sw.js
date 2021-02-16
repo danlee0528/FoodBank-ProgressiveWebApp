ddfc7d0655c2cca14cc6e481dde5e8d3460ba93b
Daniel Lee <dlee0528@hotmail.com>
2021-02-15 20:56:45 -0800

    fetch offline data

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v1';
-const dynamicCacheName = 'site-dynamic-v1'
+const staticCacheName = 'site-static-v4';
+const dynamicCacheName = 'site-dynamic-v3'
 const NUM_CACHES_TO_KEEP = 15;
 const assets = [
     '/pages/',
@@ -50,22 +50,22 @@ self.addEventListener('activate', evt =>{
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    // console.log('fetch event', evt);
-    // //offince 
-    // evt.respondWith(
-    //     caches.match(evt.request).then(cacheRes => {
-    //         return cacheRes || fetch(evt.request).then(fetchRes => {
-    //             return caches.open(dynamicCacheName).then(cache => {
-    //                 cache.put(evt.request.url, fetchRes.clone());
-    //                 limitCacheSize(dynamicCacheName, NUM_CACHES_TO_KEEP);
-
-    //                 return fetchRes;
-    //             })
-    //         });
-    //     }).catch(()=>{
-    //         if(evt.request.url.indexOf('.html') > -1 ){
-    //             return caches.match('/pages/fallback.html');
-    //         }
-    //     })
-    // );
+    if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+        //offince 
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        limitCacheSize(dynamicCacheName, NUM_CACHES_TO_KEEP);
+                        return fetchRes;
+                    })
+                });
+            }).catch(()=>{
+                if(evt.request.url.indexOf('.html') > -1 ){
+                    return caches.match('/pages/fallback.html');
+                }
+            })
+        );
+    }
 });
